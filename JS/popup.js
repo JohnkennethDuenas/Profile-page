e96b042075ece509d52478b47253f996bbd54a8b
John Kenneth Duenas <JohnKennethDuenas.20-02960@g.batstate-u.edu.ph>
2024-05-10 00:52:09 +0800

    ticket booking

---
+++ JS/popup.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById("popup");
+
+(function openPopup(){
+    popup.classList.add("open-popup")
+},0001)
+function closePopup(){
+    popup.classList.remove("open-popup")
+} 
